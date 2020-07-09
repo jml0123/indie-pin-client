cba0581311071e5db29077ccf3f22b44a6b3802c
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-07-09 12:37:58 -0400

    fixed nav styling and refactored map settings to separate file

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,6 +8,7 @@ import ArtistCard from "../ArtistCard/ArtistCard"
 import NewArtistForm from "../NewArtistForm/NewArtistForm"
 
 import "./Map.css";
+import _mapSettings from './_mapSettings';
 
 
 
@@ -23,6 +24,7 @@ export default class Map extends Component {
         pitch: 0, // pitch in degrees
         bearing: 0,
         newPin: false,
+        map: null,
         // Placeholder for now. Get from server!
         data: {
           "type": "FeatureCollection",
@@ -103,6 +105,20 @@ export default class Map extends Component {
         }
         loadScript("https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js")
 
+        const addPopup = (el, coordinates) => {
+          console.log(coordinates)
+          // Add artist card here
+          const placeholder = document.createElement('div');
+          ReactDOM.render(el, placeholder);
+      
+          marker = new mapboxgl.Popup({
+              closeOnClick: true,
+              anchor: "bottom-left"
+          })
+              .setDOMContent(placeholder)
+              .setLngLat(coordinates)
+              .addTo(this.map);
+      }
 
         this.map = new mapboxgl.Map({
             container: this.mapContainer,
@@ -113,192 +129,17 @@ export default class Map extends Component {
             pitch: this.state.pitch,
             center: [this.state.lng, this.state.lat]
         })
-        const map = this.map;
-      
-        const size = 90;
-          const pulsingDot = {
-          width: size,
-          height: size,
-          data: new Uint8Array(size * size * 4),
-           
-          // get rendering context for the map canvas when layer is added to the map
-          onAdd: function() {
-          var canvas = document.createElement('canvas');
-          canvas.width = this.width;
-          canvas.height = this.height;
-          this.context = canvas.getContext('2d');
-          },
-           
-          // called once before every frame where the icon will be used
-          render: function() {
-          var duration = 1000;
-          var t = (performance.now() % duration) / duration;
-           
-          var radius = (size / 2) * 0.3;
-          var outerRadius = (size / 2) * 0.7 * t + radius;
-          var context = this.context;
-           
-          // draw outer circle
-          context.clearRect(0, 0, this.width, this.height);
-          context.beginPath();
-          context.arc(
-          this.width / 2,
-          this.height / 2,
-          outerRadius,
-          0,
-          Math.PI * 2
-          );
-          context.fillStyle = 'rgba(125, 135, 242,' + (1 - t) + ')';
-          context.fill();
-           
-          // draw inner circle
-          context.beginPath();
-          context.arc(
-          this.width / 2,
-          this.height / 2,
-          radius,
-          0,
-          Math.PI * 2
-          );
-          context.fillStyle = 'rgba(242, 135, 125, 0.9)';
-          context.strokeStyle = 'white';
-          context.lineWidth = 2 + 4 * (1 - t);
-          context.fill();
-          context.stroke();
-           
-          // update this image's data with data from the canvas
-          this.data = context.getImageData(
-          0,
-          0,
-          this.width,
-          this.height
-          ).data;
-       
+        const map = this.map
+        _mapSettings(map)
 
-          // continuously repaint the map, resulting in the smooth animation of the dot
-          map.triggerRepaint();
-
-          // return `true` to let the map know that the image was updated
-          return true;
-          }
-          };
-
-
-        const addPopup = (el, coordinates) => {
-            console.log(coordinates)
-            // Add artist card here
-            const placeholder = document.createElement('div');
-            ReactDOM.render(el, placeholder);
-        
-            marker = new mapboxgl.Popup({
-                closeOnClick: true,
-                anchor: "bottom-left"
-            })
-                .setDOMContent(placeholder)
-                .setLngLat(coordinates)
-                .addTo(map);
-        }
-    
-        map.on('load', function() {
-            map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-            map.addSource('artists', {
-              type: 'geojson',
-              data: data
-            });
-            map.addLayer({
-                id: 'artists-heat',
-                type: 'heatmap',
-                source: 'artists',
-                maxzoom: 12,
-                paint: {
-                  // increase weight as diameter breast height increases
-                  'heatmap-weight': {
-                    property: 'popularity',
-                    type: 'exponential',
-                    stops: [
-                      [1, 0.33],
-                      [62, 2]
-                    ]
-                  },
-                  // increase intensity as zoom level increases
-                  'heatmap-intensity': {
-                    stops: [
-                      [1, 0.66],
-                      [10, 5]
-                    ]
-                  },
-                  // assign color values be applied to points depending on their density
-                  'heatmap-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['heatmap-density'],
-                    0, 'rgba(236,222,239,0)',
-                    0.2, 'rgb(208,209,230)',
-                    0.4, 'rgb(166,189,219)',
-                    0.6, 'rgb(103,169,207)',
-                    0.8, 'rgb(28,144,153)'
-                  ],
-                  // increase radius as zoom increases
-                  'heatmap-radius': {
-                    stops: [
-                      [1, 5],
-                      [3, 75]
-                    ]
-                  },
-                  // decrease opacity to transition into the circle layer
-                  'heatmap-opacity': {
-                    default: 1,
-                    stops: [
-                      [0, 1],
-                      [4.75, 0]
-                    ]
-                  },
-                }
-              }, 'waterway-label');
-              map.addLayer({
-                id: 'artists-point',
-                type: 'circle',
-                source: 'artists',
-                minzoom: 2.88,
-                paint: {
-                  // increase the radius of the circle as the zoom level and dbh value increase
-                  // Change this based on data
-                  "circle-radius": ['+', 12, ['number', ['get', 'popularity'], 12]],
-                  'circle-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'popularity'],
-                    5,
-                    'rgba(103,169,207,0.1)',
-                    10,
-                    'rgba(33,102,172,0.5)',
-                    30,
-                    'rgb(103,169,207)',
-                    50,
-                    'rgb(209,229,240)',
-                    70,
-                    'rgb(253,219,199)',
-                    90,
-                    'rgb(239,138,98)',
-                  ],
-                  'circle-stroke-color': 'white',
-                  'circle-stroke-width': 1.33,
-                  'circle-opacity': {
-                    stops: [
-                      [0, 0],
-                      [4.5, 1]
-                    ]
-                  }
-                }
-              }, 'waterway-label');
-        });
-
-        map.on('mouseenter', 'artists-point', function(e) {
-            map.getCanvas().style.cursor = 'pointer';
+        map.on('style.load', (e) => {
+          map.addSource('artists', {
+            type: 'geojson',
+            data: data
+          })
         })
-
+       
         map.on("click", (e) => {
-    
           if (marker) {
             marker.remove()
           }
@@ -309,7 +150,7 @@ export default class Map extends Component {
               togglePin()
           }
           addPopup(<NewArtistForm coordinates={[e.lngLat.lng, e.lngLat.lat]} addArtist = {onAddArtist} auth={this.props.auth}/>, [e.lngLat.lng, e.lngLat.lat])
-
+    
           var geojson = {
               type: "FeatureCollection",
               features: [{
@@ -326,11 +167,10 @@ export default class Map extends Component {
             'type': 'symbol',
             'source': 'pins',
             'layout': {
-            'icon-image': 'pulsing-dot'
+              'icon-image': 'pulsing-dot'
             }
-            });
-          
-          // Fly to point
+            });  
+        // Fly to point
         map.flyTo({
               center: [e.lngLat.lng, e.lngLat.lat],
               speed: 0.23,
@@ -343,94 +183,101 @@ export default class Map extends Component {
           })
           togglePin()
         });
-  
 
-        // Make this a re-usable function!
-        map.on('click', 'artists-point', (e) => {
-            if (marker) {
-              marker.remove()
-            }
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const artistData = e.features[0].properties
-            console.log(artistData)
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            } 
-            addPopup(<ArtistCard data={artistData} auth={this.props.auth}/>, coordinates)
-        });
-          // Make this a re-usable function!
-        map.on('mouseenter', 'artists-point', (e) => {
-          if (marker) {
-            marker.remove()
-          }
-          clearTimeout(timer)
-          const coordinates = e.features[0].geometry.coordinates.slice();
-          const artistData = e.features[0].properties
-          console.log(artistData)
-          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-          } 
-          addPopup(<ArtistCard data={artistData} auth={this.props.auth}/>, coordinates)
-        });
-        map.on('mouseleave', 'artists-point', function(e) {
-          
-          if (marker) {    
-            timer = setTimeout(() => { 
-              marker.remove()
-            }, 2900);
-          }
-        });
-
-        map.on('move', () => {
-            this.setState({
-                ...this.state,
-                lng: map.getCenter().lng.toFixed(4),
-                lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2),
-                pitch: map.getPitch(),
-                bearing: map.getBearing()
-                
-                });
-            });
-
-      map.addControl(
-        this.geocoder = new MapboxGeocoder({
-          accessToken: mapboxgl.accessToken,
-          localGeocoder: this.forwardGeocoder,
-          zoom: 14,
-          placeholder: 'Enter search e.g. Delroy Edwards',
-          mapboxgl: mapboxgl
-        }), 'bottom-left'
-      );
-
-    }
-    
-    forwardGeocoder = (query) => {
-      const matchingFeatures = [];
-      const customData = this.state.data
-      for (let i = 0; i < customData.features.length; i++) {
-      let feature = customData.features[i];
-      // handle queries with different capitalization than the source data by calling toLowerCase()
-      if (
-      feature.properties.artistName
-      .toLowerCase()
-      .search(query.toLowerCase()) !== -1
-      ) {
-      // add a tree emoji as a prefix for custom data results
-      // using carmen geojson format: https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
-      feature['place_name'] = `🎵 ${feature.properties.artistName}`
-      feature['center'] = feature.geometry.coordinates;
-      feature['place_type'] = ['artist'];
-      matchingFeatures.push(feature);
+      // Make this a re-usable function!
+    map.on('click', 'artists-point', (e) => {
+      if (marker) {
+        marker.remove()
       }
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const artistData = e.features[0].properties
+      console.log(artistData)
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      } 
+      addPopup(<ArtistCard data={artistData} auth={this.props.auth}/>, coordinates)
+  });
+    // Make this a re-usable function!
+  map.on('mouseenter', 'artists-point', (e) => {
+    if (marker) {
+      marker.remove()
+    }
+    clearTimeout(timer)
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const artistData = e.features[0].properties
+    console.log(artistData)
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    } 
+    addPopup(<ArtistCard data={artistData} auth={this.props.auth}/>, coordinates)
+  });
+  map.on('mouseleave', 'artists-point', function(e) {
+    if (marker) {    
+      timer = setTimeout(() => { 
+        marker.remove()
+      }, 2900);
+    }
+  });
+  // Consider removing this after debugging.
+  map.on('move', () => {
+      this.setState({
+            ...this.state,
+            lng: map.getCenter().lng.toFixed(4),
+            lat: map.getCenter().lat.toFixed(4),
+            zoom: map.getZoom().toFixed(2),
+            pitch: map.getPitch(),
+            bearing: map.getBearing()
+          });
+      });
+// Add to state?
+map.addControl(
+  this.geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    localGeocoder: this.forwardGeocoder,
+    zoom: 14,
+    placeholder: 'Enter search e.g. Delroy Edwards',
+    mapboxgl: mapboxgl
+  }), 'bottom-left'
+);
+
+      this.setState({
+        ...this.state,
+        map: map
+      })
+      console.log(this.state.data)
+    }
+  
+    // Update control?
+
+    getCurrentDataState = () => {
+      return this.state.data
+    }
+
+    forwardGeocoder = (query) => {
+      console.log("called")
+      const matchingFeatures = [];
+      const customData = this.getCurrentDataState()
+      for (let i = 0; i < customData.features.length; i++) {
+        let feature = customData.features[i];
+        // handle queries with different capitalization than the source data by calling toLowerCase()
+        if (feature.properties.artistName
+          .toLowerCase()
+          .search(query.toLowerCase()) !== -1)
+        {
+          // add a tree emoji as a prefix for custom data results
+          // using carmen geojson format: https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+          feature['place_name'] = `🎵 ${feature.properties.artistName}`
+          feature['center'] = feature.geometry.coordinates;
+          feature['place_type'] = ['artist'];
+          matchingFeatures.push(feature);
+        }
       }
       return matchingFeatures;
     }
+
     componentWillUnmount() {
       this.map.remove();
     }
-
- 
 
     addArtist = (newData) => {
       this.setState({
@@ -446,9 +293,6 @@ export default class Map extends Component {
       this.map.getSource('artists').setData(this.state.data)
     }
  
-
-    
-
     togglePin = () => {
         this.setState({
             ...this.state,
@@ -460,7 +304,7 @@ export default class Map extends Component {
     render(){
         return(
             <>
-            <div ref={el => this.mapContainer = el} className="mapContainer" />
+              <div ref={el => this.mapContainer = el} className="mapContainer" />
             </>
         )
     }
