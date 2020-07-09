f5e892e085d8afd8412fe0b4a992e6d74edb9cf6
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-07-09 15:25:47 -0400

    fixed nav bar on screenwidth size

--- src/views/MapView/MapView.js
+++ src/views/MapView/MapView.js
@@ -12,7 +12,7 @@ export default function MapView() {
     const auth = useContext(AuthContext)
     return(
         <div className="map-page">
-            {(!auth.token) && <div className="banner banner--smblk" onClick={e => auth.spotifyLogin("/map")}>Login with Spotify to access all features</div>}
+            {(!auth.token) && <div className="banner banner--smblk" onClick={e => auth.spotifyLogin("/map")}><p>Login with Spotify to access all features</p></div>}
             <Nav/>
             <Map auth={auth}/>
         </div>
